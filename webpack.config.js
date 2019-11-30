7789d7ad5a14f55ba2b3036fdc1d6cc682527f59
Jan Librowski <janlibrowski@gmail.com>
2019-11-30 18:24:36 +0100

    fix webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js' ],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'simple-spa',
+        libraryTarget: 'umd',
+    },
+};
